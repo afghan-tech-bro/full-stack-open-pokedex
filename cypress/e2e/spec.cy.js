586447655232cd846e89f88806e51f8385e062b8
Omid Rajabi <omid.345@gmail.com>
2022-06-09 20:29:07 -0400

    added additional test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,5 @@
+import cypress from "cypress"
+
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
@@ -9,5 +11,10 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('Navigate to pokemon page from homepage', function() {
+    cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.contains('chlorophyll')
   })
 })
